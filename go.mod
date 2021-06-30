2226698eb96ea0489ed6190adc89d3d4a1d4f3f7
张铭龙 <zhangml@idss-cn.com>
2021-06-30 13:47:12 +0800

    添加kafka读取模块

--- go.mod
+++ go.mod
@@ -3,19 +3,19 @@ module datacollector
 go 1.16
 
 require (
+	github.com/Shopify/sarama v1.29.1
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
 	github.com/bmatcuk/doublestar v1.3.4
+	github.com/bsm/sarama-cluster v2.1.15+incompatible
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/json-iterator/go v1.1.11
-	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/lestrrat-go/strftime v1.0.4
+	github.com/onsi/ginkgo v1.16.4 // indirect
+	github.com/onsi/gomega v1.13.0 // indirect
 	github.com/oschwald/geoip2-golang v1.5.0
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
 	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ reader/kafka/kafka.go
@@ -0,0 +1,277 @@
+package kafka
+
+import (
+	"datacollector/conf"
+	"datacollector/log"
+	"datacollector/reader"
+	. "datacollector/reader/config"
+	. "datacollector/utils/models"
+	"errors"
+	"fmt"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/Shopify/sarama"
+	saramaCluster "github.com/bsm/sarama-cluster"
+)
+
+var (
+	_ reader.StatsReader = &Reader{}
+	_ reader.LagReader   = &Reader{}
+	_ reader.Reader      = &Reader{}
+)
+
+func init() {
+	reader.RegisterConstructor(ModeKafka, NewReader)
+}
+
+type Reader struct {
+	lock *sync.Mutex
+	meta *reader.Meta
+	// Note: 原子操作，用于表示 reader 整体的运行状态
+	status int32
+
+	readChan <-chan *sarama.ConsumerMessage
+	errChan  <-chan error
+
+	Consumer       *saramaCluster.Consumer
+	currentOffsets map[string]map[int32]int64 // <topic,<partition,offset>>
+
+	stats     StatsInfo
+	statsLock *sync.RWMutex
+
+	groupID    string
+	Topics     []string
+	brokerList []string
+	Whence     string
+}
+
+func NewReader(meta *reader.Meta, conf conf.MapConf) (reader.Reader, error) {
+
+	whence, _ := conf.GetStringOr(KeyWhence, WhenceOldest)
+	consumerGroup, err := conf.GetString(KeyKafkaGroupID)
+	if err != nil {
+		return nil, err
+	}
+	topics, err := conf.GetStringList(KeyKafkaTopic)
+	if err != nil {
+		return nil, err
+	}
+	brokerList, _ := conf.GetStringList(KeyKafkaZookeeper)
+	/*	maxProcessingTime, _ := conf.GetStringOr(KeyKafkaMaxProcessTime, "1s")
+		maxProcessingTimeDur, err := time.ParseDuration(maxProcessingTime)
+		if err != nil {
+			return nil, fmt.Errorf("parse %s %s err %v", KeyKafkaMaxProcessTime, maxProcessingTime, err)
+		}*/
+	offsets := make(map[string]map[int32]int64)
+	for _, v := range topics {
+		offsets[v] = make(map[int32]int64)
+	}
+	sarama.Logger = log.Std
+	kr := &Reader{
+		meta:           meta,
+		groupID:        consumerGroup,
+		brokerList:     brokerList,
+		Topics:         topics,
+		Whence:         whence,
+		lock:           new(sync.Mutex),
+		statsLock:      new(sync.RWMutex),
+		currentOffsets: offsets,
+	}
+
+	config := saramaCluster.NewConfig()
+	config.Consumer.Return.Errors = true
+	/*	config.Consumer.MaxProcessingTime = maxProcessingTimeDur
+		config.Group.Return.Notifications = true*/
+
+	/*********************  kafka offset *************************/
+	/* 这里设定的offset不影响原有的offset，因为kafka client会去获取   */
+	/* zk之前的offset node记录，在没有offset node时才选择initial     */
+	switch strings.ToLower(kr.Whence) {
+	case "oldest", "":
+		config.Consumer.Offsets.Initial = sarama.OffsetOldest
+	case "newest":
+		config.Consumer.Offsets.Initial = sarama.OffsetNewest
+	default:
+		log.Warnf("Runner[%v] WARNING: Kafka consumer invalid offset '%s', using 'oldest'\n", kr.meta.RunnerName, kr.Whence)
+		config.Consumer.Offsets.Initial = sarama.OffsetOldest
+	}
+	/*************************************************************/
+
+	var consumerErr error
+	kr.Consumer, consumerErr = saramaCluster.NewConsumer(
+		kr.brokerList,
+		kr.groupID,
+		kr.Topics,
+		config,
+	)
+	if consumerErr != nil {
+		err = fmt.Errorf("runner[%v] kafka reader join group err: %v", kr.meta.RunnerName, consumerErr)
+		log.Error(err)
+		return nil, err
+	}
+	kr.readChan = kr.Consumer.Messages()
+	kr.errChan = kr.Consumer.Errors()
+	return kr, nil
+}
+
+func (r *Reader) startMarkOffset() {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			if r.isStopping() || r.hasStopped() {
+				return
+			}
+			r.markOffset()
+		}
+	}
+}
+
+func (r *Reader) isStopping() bool {
+	return atomic.LoadInt32(&r.status) == StatusStopping
+}
+
+func (r *Reader) hasStopped() bool {
+	return atomic.LoadInt32(&r.status) == StatusStopped
+}
+
+func (r *Reader) Name() string {
+	return fmt.Sprintf("KafkaReader:[%s],[%s]", strings.Join(r.Topics, ","), r.groupID)
+}
+
+func (*Reader) SetMode(_ string, _ interface{}) error {
+	return errors.New("KafkaReader does not support read mode")
+}
+
+func (r *Reader) setStatsError(err string) {
+	r.statsLock.Lock()
+	defer r.statsLock.Unlock()
+	r.stats.LastError = err
+}
+
+func (r *Reader) Source() string {
+	return fmt.Sprintf("[%s],[%s]", strings.Join(r.Topics, ","), r.groupID)
+}
+
+func (r *Reader) ReadLine() (string, error) {
+	timer := time.NewTimer(time.Second)
+	defer timer.Stop()
+	select {
+	case msg := <-r.readChan:
+		var line string
+		if msg != nil && msg.Value != nil && len(msg.Value) > 0 {
+			line = string(msg.Value)
+			r.lock.Lock()
+			if tp, ok := r.currentOffsets[msg.Topic]; ok {
+				tp[msg.Partition] = msg.Offset
+				r.currentOffsets[msg.Topic] = tp
+			} else {
+				tp := make(map[int32]int64)
+				tp[msg.Partition] = msg.Offset
+				r.currentOffsets[msg.Topic] = tp
+			}
+			r.lock.Unlock()
+		} else {
+			log.Debugf("runner[%v] Consumer read empty message: %v", r.meta.RunnerName, msg)
+		}
+		return line, nil
+	case err := <-r.errChan:
+		if err != nil {
+			err = fmt.Errorf("runner[%v] Consumer Error: %s\n", r.meta.RunnerName, err)
+			log.Error(err)
+			r.setStatsError(err.Error())
+		}
+		return "", err
+	case <-timer.C:
+	}
+
+	return "", nil
+}
+
+func (r *Reader) Status() StatsInfo {
+	r.statsLock.RLock()
+	defer r.statsLock.RUnlock()
+	return r.stats
+}
+
+func (r *Reader) Start() error {
+	go r.startMarkOffset()
+	return nil
+}
+
+func (r *Reader) Lag() (*LagInfo, error) {
+	if r.Consumer == nil {
+		return nil, errors.New("kafka consumer is closed")
+	}
+	marks := r.Consumer.HighWaterMarks()
+	rl := &LagInfo{
+		SizeUnit: "records",
+		Size:     0,
+	}
+	for _, ptv := range marks {
+		for _, v := range ptv {
+			rl.Size += v
+		}
+	}
+	r.lock.Lock()
+	for _, ptv := range r.currentOffsets {
+		for _, v := range ptv {
+			rl.Size -= v + 1 //HighWaterMarks 拿到的是下一个数据的Offset，所以实际在算size的时候多了1，要在现在扣掉。
+		}
+	}
+	r.lock.Unlock()
+	return rl, nil
+}
+
+func (r *Reader) SyncMeta() {
+	r.markOffset()
+}
+
+func (r *Reader) markOffset() {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	for topic, partOffset := range r.currentOffsets {
+		if partOffset == nil {
+			continue
+		}
+		for partition, offset := range partOffset {
+			r.Consumer.MarkOffset(&sarama.ConsumerMessage{
+				Topic:     topic,
+				Partition: partition,
+				Offset:    offset,
+			}, "")
+		}
+	}
+}
+
+func (r *Reader) stop() error {
+	r.markOffset()
+
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	err := r.Consumer.CommitOffsets()
+	if err != nil {
+		log.Errorf("Runner[%v] reader %q flush kafka offset error: %v", r.meta.RunnerName, r.Name(), err.Error())
+	}
+
+	err = r.Consumer.Close()
+	if err != nil {
+		log.Errorf("Runner[%v] reader %q close kafka consumer error: %v", r.meta.RunnerName, r.Name(), err.Error())
+	}
+
+	atomic.StoreInt32(&r.status, StatusStopped)
+	return err
+}
+
+func (r *Reader) Close() error {
+	if !atomic.CompareAndSwapInt32(&r.status, StatusRunning, StatusStopping) {
+		log.Warnf("Runner[%v] reader %q is not running, close operation ignored", r.meta.RunnerName, r.Name())
+		return nil
+	}
+	return r.stop()
+}
